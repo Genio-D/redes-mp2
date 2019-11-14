36a8a6a252c50b54d3e61fc0f361fb9e371d5e13
Diogo Dias <diogodias001@gmail.com>
2019-11-14 08:26:13 +0000

    initial commit, need good makefile

---
+++ file-receiver.c
@@ -0,0 +1,17 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include "packet-format.h"
+
+int main(int argc, char ** argv) {
+
+    int port = argv[1];
+    int window_size = argv[2];
+    
+}
---
+++ file-sender.c
@@ -0,0 +1,42 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include "packet-format.h"
+
+int main(int argc, char ** argv) {
+    struct sockaddr_in server_addr;
+	int conversion_status;
+	int connection_status;
+	struct hostent *host;
+	struct in_addr **addr_list;
+
+    char *filepath = argv[1];
+    char *host_name = argv[2];
+    int port = argv[3];
+    int window_size = argv[4];
+
+    host = gethostbyname(host_name);
+
+	if(!host)
+		fprintf(stderr, "gethostbyname: Error\n");
+
+	addr_list = (struct in_addr **)host->h_addr_list;
+	char *host_address = inet_ntoa(*addr_list[0]);
+
+	conversion_status = inet_pton(AF_INET, host_address, &server_addr.sin_addr);
+	if(!conversion_status) {
+		fprintf(stderr, "inet_pton: Error\n");
+		exit(-1);
+	}
+
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(port);
+
+    
+}
